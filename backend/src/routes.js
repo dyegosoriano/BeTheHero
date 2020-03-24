3074973795ada20a962a0d144929fea238cc06ff
Dyego Soriano <dyegodf2@gmail.com>
2020-03-24 16:10:56 -0300

    adicionando controllers de incidents

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,14 @@
 const { Router } = require('express')
 
 const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
 
 const routes = new Router()
 
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create)
+
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.create)
 
 module.exports = routes
