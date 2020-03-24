3a4ff3a88cd4ddd2d4f9104c82e1cd1f98452948
Dyego Soriano <dyegodf2@gmail.com>
2020-03-24 16:35:31 -0300

    adicionando controller e rota do Profile

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,14 @@ const { Router } = require('express')
 
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
 
 const routes = new Router()
 
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create)
+
+routes.get('/profile', ProfileController.index)
 
 routes.get('/incidents', IncidentController.index)
 routes.post('/incidents', IncidentController.create)
