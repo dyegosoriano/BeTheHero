1f39a25a4ea9786c6c4aba8c1513655e4fba3fe1
Dyego Soriano <dyegodf2@gmail.com>
2020-03-24 14:22:52 -0300

    configuração de rotas e conexão com sqlite3

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,9 @@
-console.log('Olá mundo!')
+const express = require('express')
+const routes = require('./routes')
+
+const app = express()
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(3333)
