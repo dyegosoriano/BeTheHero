24db2d214b1a7b81504b09b026918aa403b0c3db
Dyego Soriano <dyegodf2@gmail.com>
2020-03-24 16:54:42 -0300

    adicionando controller e rota de Sessions

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,8 +3,11 @@ const { Router } = require('express')
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
 const ProfileController = require('./controllers/ProfileController')
+const SessionsController = require('./controllers/SessionsController')
 
 const routes = new Router()
+
+routes.post('/sessions', SessionsController.create)
 
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create)
