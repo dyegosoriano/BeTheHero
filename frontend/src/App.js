48dfb093bab219d9408097618e9cbce077c908b5
Dyego Soriano <dyegodf2@gmail.com>
2020-03-25 16:28:18 -0300

    iniciando frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from './Header'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <h1>Olá mundo!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <h1>Be The Hero</h1>
+  );
+}
